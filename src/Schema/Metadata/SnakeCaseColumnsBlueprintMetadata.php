228ca9efb8952f1873215b6e5208d10eb4832044
MartkCz <martk494@gmail.com>
2024-04-09 17:10:41 +0200

    feat(metadata): add SnakeCaseColumnsBlueprintMetadata

---
+++ src/Schema/Metadata/SnakeCaseColumnsBlueprintMetadata.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types = 1);
+
+namespace WebChemistry\DatabaseBatch\Schema\Metadata;
+
+use WebChemistry\DatabaseBatch\Type\FieldType;
+
+final class SnakeCaseColumnsBlueprintMetadata implements BlueprintMetadata
+{
+
+	public function getName(): ?string
+	{
+		return null;
+	}
+
+	public function getTableName(): ?string
+	{
+		return null;
+	}
+
+	public function getFieldType(string $field): ?FieldType
+	{
+		return null;
+	}
+
+	public function getColumnName(string $field): ?string
+	{
+		return $this->convertCase($field);
+	}
+
+	private function convertCase(string $field): string
+	{
+		$output = '';
+		$length = strlen($field);
+		$i = 0;
+		$groupLength = 0;
+		$group = 0; // 0 - start, 1 - lowercase, 2 - uppercase
+
+		while ($i < $length) {
+			$char = $field[$i];
+
+			// lowercase
+			if ($char >= 'a' && $char <= 'z') {
+				if ($group === 2) {
+					if ($groupLength > 1) {
+						$output .= '_';
+					}
+
+					$groupLength = 0;
+				}
+
+				$output .= $char;
+				$group = 1;
+			} else {
+				if ($group === 1) {
+					$output .= '_';
+				} else {
+					$groupLength++;
+				}
+
+				$output .= strtolower($char);
+				$group = 2;
+			}
+
+			$i++;
+		}
+
+		return $output;
+	}
+
+}
